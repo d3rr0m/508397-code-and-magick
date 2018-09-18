f8cf07bf138ff2bf4c1af6c124b5a9a44fb61637
Keks <keks@htmlacademy.ru>
2018-09-18 18:40:00 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/dialog.js
@@ -0,0 +1,122 @@
+'use strict';
+
+(function () {
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var userNameInput = setup.querySelector('.setup-user-name');
+  var dialogHandler = setup.querySelector('.upload');
+
+  var INITIAL_DIALOG_COORDS = {
+    x: setup.style.top,
+    y: setup.style.left
+  };
+
+  // functions for working with popup
+  var popupEscPressHandler = function (evt) {
+    if (evt.target !== userNameInput) {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+
+    setup.style.top = INITIAL_DIALOG_COORDS.x;
+    setup.style.left = INITIAL_DIALOG_COORDS.y;
+
+    document.addEventListener('keydown', popupEscPressHandler);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', popupEscPressHandler);
+  };
+
+
+  // adding listeners for opening and closing popup
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+
+  // username validation
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      if (shift.x && shift.y) {
+        dragged = true;
+      }
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (draggedEvt) {
+          draggedEvt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  window.dialog = {
+    setup: setup
+  };
+})();
